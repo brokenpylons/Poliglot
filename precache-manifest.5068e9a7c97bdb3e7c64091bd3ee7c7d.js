0af11c3b5ed91015a94078d10a0e6f23651d1be6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-12 10:18:13 +0000

    Deploy poliglot.um.si to github.com/brokenpylons/Poliglot.git:gh-pages

---
+++ precache-manifest.5068e9a7c97bdb3e7c64091bd3ee7c7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c45b994d3f564739b098",
+    "url": "/static/css/main.7a46daf2.chunk.css"
+  },
+  {
+    "revision": "c45b994d3f564739b098",
+    "url": "/static/js/main.af27ffe3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "808d56ea983e4028aa64",
+    "url": "/static/js/2.90171783.chunk.js"
+  },
+  {
+    "revision": "0da3d4637882b551ddc59baba63b6474",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0f78ce9eefd67697f6ccba26606f7f31.js"
+  "/precache-manifest.5068e9a7c97bdb3e7c64091bd3ee7c7d.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
